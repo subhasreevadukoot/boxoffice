3550db47e88fde5d435a613cbe5f1e1ad1b318b9
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-05-25 09:41:18 +0100

    Displaying API results custom render functions
    Displayed API results and refactored fetch code.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,39 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
-
+import {apiGet} from '../misc/config'
 const Home = () => {
     const [input,setInput]=useState('');
-
+    const [results,setResults]=useState('');
     const onInputChange = (e)=>{
         setInput(e.target.value);
     }
     const onSearch = () =>{
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-        .then(response =>response.json())
+        apiGet(`/search/shows?q=${input}`).then(response =>response.json())
         .then(result=>{
-
+            setResults(result)
         })
+      
+       
     }
     const onKeyDown=(e)=>{
         if (e.keyCode ===13){
             onSearch();
         }
     }
+    const renderResults = () =>{
+        if(results && results.length===0){
+            return <div>No results</div>
+        }
+        else if (results && results.length>0){
+            return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+        }
+        return null
+    }
     return (
        <MainPageLayout>
           <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
           <button type='button' onClick={onSearch}>Search</button>
+          {renderResults()}
        </MainPageLayout>
     )
 }
