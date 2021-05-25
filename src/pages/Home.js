c7c2a0dd7e5dd06a3a845744ee334a46d7c9fa39
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-05-25 17:19:37 +0100

    Added search with actors option
    Implemented radio buttons for searching either shows or actors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,90 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
-import {apiGet} from '../misc/config'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+
 const Home = () => {
-    const [input,setInput]=useState('');
-    const [results,setResults]=useState('');
-    const onInputChange = (e)=>{
-        setInput(e.target.value);
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
-    const onSearch = () =>{
-        apiGet(`/search/shows?q=${input}`).then(response =>response.json())
-        .then(result=>{
-            setResults(result)
-        })
-      
-       
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
-    const onKeyDown=(e)=>{
-        if (e.keyCode ===13){
-            onSearch();
-        }
+
+    if (results && results.length > 0) {
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
-    const renderResults = () =>{
-        if(results && results.length===0){
-            return <div>No results</div>
-        }
-        else if (results && results.length>0){
-            return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
-        }
-        return null
-    }
-    return (
-       <MainPageLayout>
-          <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
-          <button type='button' onClick={onSearch}>Search</button>
-          {renderResults()}
-       </MainPageLayout>
-    )
-}
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
 
 export default Home;
