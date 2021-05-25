3cfda0b849aeafa999614ba44c5f052a0a892869
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-05-25 09:12:42 +0100

    Added search functionality
    Added API and search functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+    const [input,setInput]=useState('');
+
+    const onInputChange = (e)=>{
+        setInput(e.target.value);
+    }
+    const onSearch = () =>{
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+        .then(response =>response.json())
+        .then(result=>{
+
+        })
+    }
+    const onKeyDown=(e)=>{
+        if (e.keyCode ===13){
+            onSearch();
+        }
+    }
     return (
        <MainPageLayout>
-           home
+          <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+          <button type='button' onClick={onSearch}>Search</button>
        </MainPageLayout>
     )
 }
 
-export default Home
+export default Home;
