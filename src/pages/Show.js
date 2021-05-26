d480d10cc0fb6deb10b3b3008a51ffadaf08517d
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-05-26 09:42:43 +0100

    Added loading and error

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -4,17 +4,33 @@ import { apiGet } from '../misc/config';
 const Show = () => {
     const {id} = useParams();
     const [show, setShow] = useState(null);
-
+    const [error,setError]=useState(null);
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
+        let isMounted=true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-            setShow(results);
+            if(isMounted){
+            setTimeout(() => {
+                setShow(results);
+                setIsLoading(false)
+            }, 2000);
+        }
+          }).catch(error=>{
+              setError(error.message);
+              setIsLoading(false)
           });
        
-        /*for using when component unmounts
+        /*for using when component unmounts*/
         return () => {
-            cleanup
-        }*/
+          isMounted=false;
+        }
     }, [id])
+    if(isLoading){
+        return <div>Data is being loaded</div>
+    }
+    if(error){
+        return <div>Error: {error}</div>
+    }
     return (
         <div>
             
