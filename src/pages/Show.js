7c7c60784c233efd579be39db8235cd905a557dc
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-05-30 08:10:36 +0100

    Refactor: replaced useState with useReducer hook

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,43 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useReducer} from 'react'
 import {useParams} from 'react-router-dom'
 import { apiGet } from '../misc/config';
 const Show = () => {
     const {id} = useParams();
-    const [show, setShow] = useState(null);
+
+    const reducer = (prevState,action) => {
+        switch(action.type){
+            case 'FETCH_SUCCESS':{
+                return {...prevState,isLoading:false,show:action.show}
+            }
+            case 'FETCH_FAILED':{
+                return {...prevState,isLoading:false,error:action.error}
+            }
+            default:
+                return null
+        }
+
+    }
+ /*    const [show, setShow] = useState(null);
     const [error,setError]=useState(null);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true) */
+    const initialState={
+        show:null,
+        isLoading:true,
+        error:null
+    }
+
+    const[{show,isLoading,error},dispatch]= useReducer(reducer,initialState)
     useEffect(() => {
         let isMounted=true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
             if(isMounted){
-            setTimeout(() => {
-                setShow(results);
-                setIsLoading(false)
-            }, 2000);
+                dispatch({type:'FETCH_SUCCESS',show:results})
         }
           }).catch(error=>{
-              setError(error.message);
-              setIsLoading(false)
+            dispatch({type:'FETCH_FAIL',error:error.message})
+
+             /*  setError(error.message);
+              setIsLoading(false) */
           });
        
         /*for using when component unmounts*/
